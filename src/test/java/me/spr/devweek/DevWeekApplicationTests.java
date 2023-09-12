f4816d87682127af0938b59e34b84b2a56086840
isaacgdias <isaacbogui98@gmail.com>
2023-09-12 18:28:59 -0300

    estrutura do projeto

---
+++ src/test/java/me/spr/devweek/DevWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package me.spr.devweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
